dda06b17d3f44d3a0d1fbc7fb71e9aaca6b5941b
Firenzo <firenzo@MacBook-Pro-van-Firenzo.local>
2023-10-31 16:04:35 +0100

    test environment variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from "astro/config";
 
+const createAstroConfig = () => {
+  console.log(`${process.env.NODE_ENV} mpp`);
+  if (process.env.NODE_ENV === "prd") {
+    return {
+      site: process.env.SITE_URL,
+      base: process.env.BASE_URL,
+    };
+  }
+  return {};
+};
+let astroConfig = createAstroConfig();
+
 // https://astro.build/config
-export default defineConfig({
-  site: "https://firenzo.github.io",
-  base: "/contentstack-certification-assignement",
-});
+export default defineConfig(astroConfig);
