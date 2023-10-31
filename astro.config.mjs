bc7376c9b9b8a52e7b3ac0f931d768cb8f35067a
Firenzo <firenzo@MacBook-Pro-van-Firenzo.local>
2023-10-31 16:12:19 +0100

    test environment variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 
 const createAstroConfig = () => {
-  console.log(`${process.env.NODE_ENV} mpp`);
   if (process.env.NODE_ENV === "prd") {
     return {
       site: process.env.SITE_URL,
