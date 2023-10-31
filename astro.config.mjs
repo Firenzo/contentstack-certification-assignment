1f698f52c45b37bae59c24c100f4b4f1e03456cc
Firenzo <firenzo@MacBook-Pro-van-Firenzo.local>
2023-10-31 15:11:33 +0100

    add astro config for hosting site on subfolder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://firenzo.github.io",
+  base: "/contentstack-certification-assignement",
+});
